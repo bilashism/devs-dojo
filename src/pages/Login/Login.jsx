dfffe8ce45ddb76ee7416a57a70554c1070faee6
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-10-26 23:09:53 +0600

    login system added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,81 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useRef, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import loginImg from "../../images/testimonial-1.png";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
+import { AuthContext } from "../../authentication/AuthProvider/AuthProvider";
+import { GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+import toast from "react-hot-toast";
 
 const Login = () => {
+  const defaultFeedback = `We'll never share your details.`;
+
+  const navigate = useNavigate();
+  const [feedback, setFeedback] = useState(defaultFeedback);
+  const userEmailRef = useRef();
+  const userPasswordRef = useRef();
+  const location = useLocation();
+  const from = location?.state?.from?.pathName || "/";
+
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
+
+  const { userLogIn, providerLogin, setAuthLoading } = useContext(AuthContext);
+
+  /**
+   * The handleProviderLogin function takes a provider as an argument and then calls the providerLogin
+   * function with the provider as an argument. If the promise resolves, it will display a toast message.
+   * If the promise rejects, it will set the feedback state to the error message and log the error to the
+   * console.
+   */
+  const handleProviderLogin = provider => {
+    providerLogin(provider)
+      .then(data => {
+        toast.success("Login successful! 🎉");
+      })
+      .catch(err => {
+        setFeedback(err?.message.replace("Firebase: ", ""));
+        console.error(err);
+      });
+  };
+
+  /**
+   * When the user clicks the submit button, prevent the default action, grab the email and password from
+   * the form, and if the email and password are not empty, call the userLogIn function, which is
+   * imported from the firebase.js file, and pass in the email and password as arguments. If the
+   * userLogIn function is successful, reset the form, set the feedback to the default feedback, show a
+   * success message, and navigate to the home page. If the userLogIn function is unsuccessful, set the
+   * feedback to the error message, and set the authLoading to false.
+   */
+  const handleUserLogIn = ev => {
+    ev.preventDefault();
+    const form = ev.target;
+    const email = userEmailRef.current.value;
+    const password = userPasswordRef.current.value;
+
+    if (email && password) {
+      userLogIn(email, password)
+        .then(result => {
+          console.log(result);
+          form.reset();
+          setFeedback(defaultFeedback);
+          toast.success("Login successful! 🎉");
+          navigate("/");
+        })
+        .catch(err => {
+          setFeedback(err?.message.replace("Firebase: ", ""));
+          console.error(err);
+        })
+        .finally(() => {
+          setAuthLoading(false);
+        });
+    }
+  };
+
   return (
     <section className="py-8 lg:py-4">
       <div className="container mx-auto px-4 grid gap-8 items-center lg:grid-cols-2 lg:min-h-screen">
+        {/* image section */}
         <figure className="order-first lg:order-none rounded-full bg-red-500 aspect-square">
           <picture className="inline-flex  ">
             <source srcSet={loginImg} />
@@ -24,8 +92,9 @@ const Login = () => {
           </picture>
         </figure>
 
+        {/* form section  */}
         <div className="">
-          <form className="lg:max-w-lg mx-auto">
+          <form onSubmit={handleUserLogIn} className="lg:max-w-lg mx-auto">
             <div className="relative z-0 mb-6 w-full group">
               <input
                 type="email"
@@ -34,6 +103,7 @@ const Login = () => {
                 placeholder=" "
                 required=""
                 autoComplete="username"
+                ref={userEmailRef}
               />
               <label
                 htmlFor="user-email"
@@ -49,6 +119,7 @@ const Login = () => {
                 placeholder=" "
                 required=""
                 autoComplete="current-password"
+                ref={userPasswordRef}
               />
               <label
                 htmlFor="user-password"
@@ -57,7 +128,7 @@ const Login = () => {
               </label>
             </div>
             <div className="py-2 text-center flex flex-col gap-5">
-              <p className="">We'll never share your details.</p>
+              <p className="">{feedback}</p>
 
               <button
                 type="submit"
@@ -73,11 +144,14 @@ const Login = () => {
               <hr className="border mb-6 border-red-500" />
               <div className="text-center flex flex-col gap-4">
                 <button
+                  onClick={() => handleProviderLogin(googleProvider)}
                   type="button"
                   className="inline-flex items-center justify-center text-center gap-2 p-3 bg-slate-200 hover:bg-slate-500 hover:text-white transition">
                   <FcGoogle /> Log in with Google
                 </button>
+
                 <button
+                  onClick={() => handleProviderLogin(githubProvider)}
                   type="button"
                   className="inline-flex items-center justify-center text-center gap-2 p-3 bg-slate-200 hover:bg-slate-500 hover:text-white transition">
                   <FaGithub /> Log in with Github
