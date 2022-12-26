c5667d298019c87d94d8846e04846550e83cbff4
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-26 14:33:52 +0600

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,8 +35,7 @@ const Footer = () => {
                 Contact Us
               </h1>
               <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-                gentrify.
+                Feel free to share your concern with us
               </p>
             </div>
             <div className="lg:w-1/2 md:w-2/3 mx-auto">
